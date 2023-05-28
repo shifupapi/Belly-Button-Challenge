a446f0e1a4237b59e79d6a7ae8a87c4746784295
Shifupapi <shifupapi@Seans-MacBook-Air.local>
2023-05-27 22:19:36 -0400

    inital code 1

--- static/js/app.js
+++ static/js/app.js
@@ -22,7 +22,7 @@ console.log(sample_one);
 buildMetadata(sample_one);
 buildBarChart(sample_one);
 buildBubbleChart(sample_one);
-// buildGaugeChart(sample_one);
+buildGaugeChart(sample_one);
 
 
 });
@@ -35,7 +35,7 @@ function buildMetadata(sample) {
 
         let value = metadata.filter(result => result.id == sample);
 
-        console.log(value)
+        console.log(value);
 
         let valueData = value[0];
 
@@ -81,7 +81,7 @@ function buildBarChart(sample) {
 
         Plotly.newPlot("bar", [trace], layout)
     });
-}
+};
 
 function buildBubbleChart(sample) {
 
@@ -127,8 +127,62 @@ function optionChanged(value) {
     buildMetadata(value);
     buildBarChart(value);
     buildBubbleChart(value);
-    buildGaugeChart(sample_one);
+    buildGaugeChart(value);
 
+};
+
+
+function buildGaugeChart(sample) {
+
+    
+    d3.json(url).then((data) => {
+ 
+        let metadata = data.metadata;
+
+        let value = metadata.filter(result => result.id == sample);
+
+        console.log(value);
+
+        let valueData = value[0];
+        
+        let washFrequency = Object.values(valueData)[6]; 
+        
+        let trace2 = {
+            value: washFrequency,
+            domain: {x: [0,1], y: [0,1]},
+            title: {
+                text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+                font: {color: "black", size: 10}
+            },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: {range: [0,10]},
+                steps: [
+                    {range: [0, 1], color: "rgba(255, 255,255, .5"},
+                    {range: [1, 2], color: "rgba(232, 226, 202, .5)"},
+                    {range: [2, 3], color: "rgba(210, 206, 145, .5)"},
+                    {range: [3, 4], color:  "rgba(202, 209, 95, .5)"},
+                    {range: [4, 5], color:  "rgba(184, 205, 68, .5)"},
+                    {range: [5, 6], color: "rgba(170, 202, 42, .5)"},
+                    {range: [6, 7], color: "rgba(142, 178, 35 , .5)"},
+                    {range: [7, 8], color:  "rgba(110, 154, 22, .5)"},
+                    {range: [8, 9], color: "rgba(50, 143, 10, 0.5)"},
+                    {range: [9, 10], color: "rgba(0, 127, 0, .5)"},
+                ]
+            } 
+        };
+
+       
+        let layout = {
+            width: 400, 
+            height: 300,
+            margin: {t: 0, b:0}
+        };
+
+        
+        Plotly.newPlot("gauge", [trace2], layout)
+    });
 };
 
 
